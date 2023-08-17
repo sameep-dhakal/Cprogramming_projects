4043b841f0c8b9d5de58aa9ad626f866ccfad3ce
Sugam Sinjali <sugam.079bct080@acem.edu.np>
2023-08-17 17:30:17 +0545

    Committed

--- hangman game/pratik.c
+++ hangman game/pratik.c
@@ -1,83 +1,147 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
 
-int main()
-{
-int i,j,c,count=0,ans=0,flag=0,*ptr;
-char a[1][10]={"dogodo"};
-char b[10],alpha;
-char d='_';
-c=strlen(&a[0][0]);
-//printf("\n\t\t**************\n\n\t\t\t");
-printf("\n\n\t\t\t ** HANGMAN ** \n");
-   printf("\n\t\t\t**************\t\t\t");
-      printf("\n\t\t\t..............\n\n\t\t\t  ");
-for(j=0;j<c;j++)
-     { printf("%c ",d);
-     b[j]=d;}
-     //printf("\n\n\t\t*****************");
-     printf("\n\n\t\t\t..............\t\t\t");
-      printf("\n\n\t\t\t**************");
-// for(j=0;j<c;j++) printf("\n %c",b[j]); //{     ptr=&b[j];
-    //  printf("\n %c",*ptr); }
+#define WORDS 10
+#define WORDLEN 40
+#define CHANCE 6
 
-while (count<6)//||(ans<c))
- {
- flag=0;
-      printf("\n\n\n\n\n\n\t enter a char");
-      alpha=getche();
-     //   printf("%c",alpha);
-      for(i=0;i<c;i++)
-      {
-         if (alpha==a[0][i])
-         {   b[i]=a[0][i];
-         flag=1;
-         ans++; }
-         //else if(b[i]!=d) b[i]=a[0][i];
-         //else b[i]=d;
-         //  printf("\n u r correct");  }
-         //  if(ans==c) goto man; }
-         //printf("\n ans=%d",ans);}
+bool srand_called = false;
 
-         //printf("\n count=%d",count);
+int i_rnd(int i) {
+    if (!srand_called) {
+        srand(time(NULL) << 10);
+        srand_called = true;
+    }
+    return rand() % i;
+}
 
-      }
+char* decrypt(char* code) {
+	int hash = ((strlen(code) - 3) / 3) + 2;
+	char* decrypt = malloc(hash);
+	char* toFree = decrypt;
+	char* word = code;
+	for (int ch = *code; ch != '\0'; ch = *(++code))
+	{
+		if((code - word + 2) % 3  == 1){
+			*(decrypt++) = ch - (word - code + 1) - hash;
+		}
+	}
+	*decrypt = '\0';
+	return toFree;
+}
 
-      printf("\n\n\t\t\t ** HANGMAN ** \n");
-      printf("\n\t\t\t**************\t\t\t");
-      printf("\n\t\t\t..............\n\n\t\t\t  ");
-      for(i=0;i<c;i++)
-      printf("%c ",b[i]);
-      printf("\n\n\t\t\t..............\t\t\t");
-      printf("\n\n\t\t\t**************");
+void printBody(int mistakes, char* body) {
+	printf("\tMistakes :%d\n", mistakes);
+	switch(mistakes) {
 
-      if(flag==0)
-      {count++;
-      printf("\n\n\n\n\t\t%c is a wrong guess",alpha);
-      printf("\n\n\t\t (u have %d more guesses)",6-count);}
+		case 6: body[6] = '\\'; break;
+		case 5: body[5] = '/'; break;
+		case 4: body[4] = '\\'; break;
+		case 3: body[3] = '|'; break;
+		case 2: body[2] = '/'; break;
+		case 1: body[1] = ')', body[0] = '('; break;
+		default: break;
 
-      else{
-      printf("\n\n\t\t u r correct!!");
-      printf("\n\t\t( u have %d more words to go)",c-ans);}
-      //  printf("\n ans=%d",ans);
-      //  printf("\n count=%d",count);
-      if(ans==c) break;
- }
- if(ans==c) printf("\n\n\n\t you won");
- else printf("\n\n\n\t\t  u lose.\n\n \t\t **SORRY  U R HANGED**");
+	}
 
-     /*   if(output[i]!='_')
-      output[i]=a[0][i];
-      elseif(b[j]==a[0][i])
-      output[i]==a[0][i];
-      else
-      output[i]='_';
-      }
-      for(i=0;i<c;i++)
-      printf("%c",output[i])
+	printf("\t _________\n"
+	       "\t|         |\n"
+	       "\t|        %c %c\n"
+	       "\t|        %c%c%c\n"
+	       "\t|        %c %c\n"
+	       "\t|             \n"
+	       "\t|             ", body[0], body[1], body[2],
+	       body[3], body[4], body[5], body[6]);
+}
 
- }      */
-getch();
+void printWord(char* guess, int len) {
+	printf("\t");
+	for (int i = 0; i < len; ++i)
+	{
+		printf("%c ", guess[i]);
+	}
+	printf("\n\n");
+}
 
+int main() {
+
+	printf("\n\t Be aware you can be hanged!!.");
+
+	printf("\n\n\t Rules : ");
+	printf("\n\t - Maximum 6 mistakes are allowed.");
+	printf("\n\t - All alphabet are in lower case.");
+	printf("\n\t - All words are name of very popular Websites. eg. Google");
+	printf("\n\t - If you enjoy continue, otherwise close it.");
+
+	printf("\n\t Syntax : Alphabet");
+	printf("\n\t Example : a \n\n");
+
+	char values[WORDS][WORDLEN] = {"N~mqOlJ^tZletXodeYgs","gCnDIfFQe^CdP^^B{hZpeLA^hv","7urtrtwQv{dt`>^}FaR]i]XUug^GI",
+									"aSwfXsxOsWAlXScVQmjAWJG","cruD=idduvUdr=gmcauCmg]","BQt`zncypFVjvIaTl]u=_?Aa}F",
+									"iLvkKdT`yu~mWj[^gcO|","jSiLyzJ=vPmnv^`N]^>ViAC^z_","xo|RqqhO|nNstjmzfiuoiFfhwtdh~",
+									"OHkttvxdp|[nnW]Drgaomdq"};
+	char *body = malloc(CHANCE+1);
+
+	int id = i_rnd(WORDS);
+	char *word = decrypt(values[id]);
+	int len = strlen(word);
+	char *guessed = malloc(len);
+	char falseWord[CHANCE];
+
+	memset(body, ' ', CHANCE+1);
+	memset(guessed, '_', len);
+	char guess;
+	bool found;
+	char* win;
+
+	int mistakes = 0;
+	setvbuf(stdin, NULL, _IONBF, 0);
+
+	do {
+
+		found = false;
+		printf("\n\n");
+		printBody(mistakes, body);
+		printf("\n\n");
+		printf("\tFalse Letters : ");
+		if(mistakes == 0) printf("None\n");
+		for (int i = 0; i < mistakes; ++i)
+		{
+			printf("%c", falseWord[i]);
+		}
+		printf("\n\n");
+		printWord(guessed, len);
+		printf("\tGive me a alphabet in lower case : ");
+		do {scanf("%c",&guess);} while ( getchar() != '\n' );
+		for (int i = 0; i < len; ++i)
+		{
+			if(word[i] == guess) {
+				found = true;
+				guessed[i] = guess;
+			}
+		}
+		if(!found) {
+			falseWord[mistakes] = guess;
+			mistakes += 1;
+		}
+		win = strchr(guessed, '_');
+	}while(mistakes < CHANCE && win != NULL);
+
+	if(win == NULL) {
+		printf("\n");
+		printWord(guessed, len);
+		printf("\n\tCongrats! You have won : %s\n\n", word);
+	} else {
+		printf("\n");
+		printBody(mistakes, body);
+		printf("\n\n\tBetter try next time. Word was %s\n\n", word);
+	}
+
+	free(body);
+	free(word);
+	free(guessed);
+	return EXIT_SUCCESS;
 }
